9e0c2a68a3560c1d581f935c3f5c812823edd916
Shambhavi Goyal <sgoyal3@buffalo.edu>
2025-04-19 21:04:56 -0400

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import Header from "./Header.jsx"
+import Main from "./Main.jsx"
+export default function App() {
+  return (
+    <>
+       <Header />
+       <Main />
+    </>
+  )
+}
---
+++ src/Header.jsx
@@ -0,0 +1,11 @@
+import ChefClaudeLogo from "./images/chef-claude-icon.png"
+
+export default function Header() {
+    return(
+        <header>
+            <img src={ChefClaudeLogo} alt="Chef Claude Logo" />
+            <h1>Chef Claude</h1>
+        </header>
+
+    )
+}
